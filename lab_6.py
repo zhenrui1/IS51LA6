69e0f671d19e79eed93ca254ef2987b4be7a561a
zhenrui1 <zhenrui1219@mail.fresnostate.edu>
2022-05-12 22:06:09 -0700

    finish code

--- lab_6.py
+++ lab_6.py
@@ -31,3 +31,28 @@ loop three times
   print the output (fruit) to the user
 """
 
+import random 
+
+def main():
+  for i in range (0, 3):
+    spin()
+
+def spin():
+  rand_num = random.randint(1, 20)
+  output = ""
+  if(rand_num > 15):
+    output = "Cherries"
+  elif(rand_num > 10):
+    output = "Orange"
+  elif(rand_num > 5):
+    output = "Plum"
+  elif(rand_num > 2):
+    output = "Melon"
+  elif(rand_num > 1):
+    output = "Bell"
+  else:
+    output = "Bar"
+
+  print(output, end=" ")
+
+main()
